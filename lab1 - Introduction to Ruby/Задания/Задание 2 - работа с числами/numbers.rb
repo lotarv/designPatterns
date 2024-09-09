392ff4ba42aa2b6313adce5677682264ced7630e
lotarv <lotarev200445@gmail.com>
2024-09-09 19:14:26 +0300

    2.3 done

--- lab1 - Introduction to Ruby/Задания/Задание 2 - работа с числами/numbers.rb
+++ lab1 - Introduction to Ruby/Задания/Задание 2 - работа с числами/numbers.rb
@@ -18,3 +18,48 @@ def max_prime_divisor(number)  #Метод 1 - Найти максимальны
     return max_divisor
 end
 
+def mult_of_digits_not_divide_by_5(number) #Метод 2 - Найти произведение цифр числа, не делящихся на 5
+    result = 1
+    numberCpy = number
+    while numberCpy > 0
+        digit = numberCpy % 10
+        result *= digit if (digit % 5 != 0)
+        numberCpy /= 10
+    end
+    return result
+end
+
+def gdc_special(number)  #Метод 3 - Найти НОД максимального нечетного непростого делителя числа и произведения цифр данного числа
+    #Вычисляем максимальный нечетный непростой делитель
+    max_odd_nonprime_divisor = 1
+    for i in 2..number
+        next if i % 2 == 0  # исключаем четные
+        next if (number % i != 0)  #исключаем не делители
+        next if (is_prime(i))  #исключаем простые числа
+        max_odd_nonprime_divisor = i
+    end
+    
+    
+    #Вычисляем произведение чисел данного числа
+    multiple_of_digits = 1
+
+    while number > 0
+        multiple_of_digits *= number % 10
+        number /= 10
+    end
+
+    #Вычисляем НОД - наибольший общий делитель
+    a = max_odd_nonprime_divisor
+    b = multiple_of_digits
+
+    while b != 0
+        a,b = b, a % b
+    end
+
+    return a
+end
+
+
+puts gdc_special(545)
+
+
