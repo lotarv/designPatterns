569a65e165d8919556c742c3c9e479298a30040c
Qullfie <lotarev200445@gmail.com>
2024-09-14 10:02:42 +0300

    lab2 1.3-4. made attr_accessors to shorten code and avoid repeats

--- lab2 - Classes/main.rb
+++ lab2 - Classes/main.rb
@@ -4,6 +4,4 @@ student1 = Student.new(1, "Sergey", "Lotarev", "Urievich", '88005553535', 'lotar
 student2 = Student.new(2, "Amal", "Blyagoz", "Hazretovich", '89003335555', 'lamafout', 'amalblyagoz@yandex.ru', 'lamafout');
 student3 = Student.new(3, "Filipp", "Matyuha", "Andreevich", '8325854233', 'serenity_flaim', 'serenity_flaim@yandex.ru', 'serenity_flaim');
 
-student1.showInfo()
-student2.showInfo()
-student3.showInfo()
+print student1.name
