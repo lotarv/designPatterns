97020a8c1ff4ae60fc37015158122a3c957dfe3f
Qullfie <lotarev200445@gmail.com>
2024-09-14 09:18:05 +0300

    lab2 made Student class with million getters and setters

---
+++ lab2 - Classes/student.rb
@@ -0,0 +1,87 @@
+class Student
+    def initialize(id, name, surname, middle_name, phone = '', telegram = '', email = '', git = '')
+        @id = id
+        @name = name
+        @surname = surname
+        @middle_name = middle_name
+        @phone = phone
+        @telegram = telegram
+        @email = email
+        @git = git
+    end
+
+    #getter & setter for @id
+    def id
+        @id
+    end
+
+    def id=(value)
+        @id = value
+    end
+
+    #getter & setter for @name
+    def name
+        @name
+    end
+
+    def name=(value)
+        @name = value
+    end
+
+    #getter & setter for @surname
+    def surname
+        @surname
+    end
+
+    def surname=(value)
+        @surname = value
+    end
+
+    #getter & setter for @middle_name
+    def middle_name
+        @middle_name
+    end
+
+    def middle_name=(value)
+        @middle_name = value
+    end
+
+    #getter & setter for @phone
+    def phone
+        @phone
+    end
+
+    def phone=(value)
+        @phone = value
+    end
+
+     #getter & setter for @telegram
+    def telegram
+        @telegram
+    end
+
+    def telegram=(value)
+        @telegram = value
+    end
+
+     #getter & setter for @email
+    def email
+        @email
+    end
+
+    def email=(value)
+        @email = value
+    end
+
+     #getter & setter for @git
+    def git
+        @git
+    end
+
+    def git=(value)
+        @git = value
+    end
+end
+
+student1 = Student.new(1,"Sergey", "Lotarev", "Urievich")
+
