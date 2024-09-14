2eba925aa19b286889edf38a82c1ffabad701900
Qullfie <lotarev200445@gmail.com>
2024-09-14 10:02:42 +0300

    lab2 2.3-4 made attr_accessors to shorten code and avoid repeats

--- lab2 - Classes/student.rb
+++ lab2 - Classes/student.rb
@@ -1,4 +1,5 @@
 class Student
+    attr_accessor :id, :name,:surname, :middle_name,:phone,:telegram,:email,:git
     def initialize(id, name, surname, middle_name, phone = '', telegram = '', email = '', git = '')
         @id = id
         @name = name
@@ -18,78 +19,6 @@ class Student
         puts "email: #{@email}" if @email
         puts "git : #{@git}" if @github
         puts "***********************"
-    end
-
-    #getter & setter for @id
-    def id
-        @id
-    end
-
-    def id=(value)
-        @id = value
-    end
-
-    #getter & setter for @name
-    def name
-        @name
-    end
-
-    def name=(value)
-        @name = value
-    end
-
-    #getter & setter for @surname
-    def surname
-        @surname
-    end
-
-    def surname=(value)
-        @surname = value
-    end
-
-    #getter & setter for @middle_name
-    def middle_name
-        @middle_name
-    end
-
-    def middle_name=(value)
-        @middle_name = value
-    end
-
-    #getter & setter for @phone
-    def phone
-        @phone
-    end
-
-    def phone=(value)
-        @phone = value
-    end
-
-     #getter & setter for @telegram
-    def telegram
-        @telegram
-    end
-
-    def telegram=(value)
-        @telegram = value
-    end
-
-     #getter & setter for @email
-    def email
-        @email
-    end
-
-    def email=(value)
-        @email = value
-    end
-
-     #getter & setter for @git
-    def git
-        @git
-    end
-
-    def git=(value)
-        @git = value
     end
 end
 
