fd32bc111ea7d96547d34ddb022f274159ba0abb
lotarv <lotarev200445@gmail.com>
2024-10-07 13:33:40 +0300

    lab3 1 check_rotation

--- lab3 - develop_concepts/lists.rb
+++ lab3 - develop_concepts/lists.rb
@@ -2,7 +2,7 @@ def menu
     puts "Выберите задачу для решения:"
     puts "1. Задача 1 - Найти элементы, расположенные перед последним минимальным"
     puts "2. Задача 2 - Найти элементы, расположенные после первого максимального"
-    puts "3. Задача 2 - Найти элементы, расположенные после первого максимального"
+    puts "3. Задача 3 - Проверить, чередуются ли в массиве положительные и отрицательные числа"
     puts "4. Задача 2 - Найти элементы, расположенные после первого максимального"
     puts "5. Задача 2 - Найти элементы, расположенные после первого максимального"
     puts "6. Выход"
@@ -20,7 +20,7 @@ def find_elements_before_last_min(arr) # Задача 1.9
     min_index = arr.rindex(min_value)
     result = arr.slice(0, min_index)
 
-    yield(result) if block_given? 
+    puts "Результат: #{result}"
 end
 
 def find_elements_after_first_max(arr) #Задача 1.21
@@ -30,31 +30,32 @@ def find_elements_after_first_max(arr) #Задача 1.21
     print max_index
     result = arr.slice(max_index + 1, arr.length)
 
-    yield(result) if block_given?
+    puts "Результат: #{result}"
 end
 
-def find_elements_after_first_max(arr) #Задача 1.21
-    puts "Исходный массив: #{arr}" 
-    max_value = arr.max
-    max_index = arr.index(max_value)
-    print max_index
-    result = arr.slice(max_index + 1, arr.length)
+def check_rotation(arr) #Задача 1.33
+    
+    result = arr.each_cons(2).all? {|a,b| (a.positive? && b.negative?) || (a.negative? && b.positive?)}
 
-    yield(result) if block_given?
+    if result
+        puts "Числа чередуются"
+    else
+        puts "Числа не чередуются"
+    end
 end
 
-arr = [9,5,3,7,1,4,1,9]
+# arr = [9,5,3,7,1,4,1,9]
+arr = [1,-1,2,-2,3,-3]
 loop do
     menu
     choice = gets.chomp.to_i
     case choice
     when 1
-        find_elements_before_last_min(arr) {|result| puts "Результат: #{result}"} # Пример блока для задачи 1
+        find_elements_before_last_min(arr)
     when 2
-    f   ind_elements_after_first_max(arr) {|result| puts "Результат: #{result}"}  # Пример блока для задачи 2
+        find_elements_after_first_max(arr)  
     when 3
-        data = read_from_keyboard
-    puts "Вы ввели: #{data}"
+        check_rotation(arr)
     when 4
         data = read_from_file
         puts "Содержимое файла: #{data}" if data
