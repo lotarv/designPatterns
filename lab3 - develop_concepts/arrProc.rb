4a06545506b79e80ae1017f7ee9e974fe3968c68
lotarv <lotarev200445@gmail.com>
2024-10-15 18:59:27 +0300

    lab2 2.4 chunk

--- lab3 - develop_concepts/arrProc.rb
+++ lab3 - develop_concepts/arrProc.rb
@@ -69,25 +69,42 @@ class ArrProc
         return false
     end
 
-    def chunk
+    def chunk(&block)
+        collections = []  # Массив для хранения групп
+        current_group = []  # Текущая группа
+        previous_value = nil  # Хранит предыдущее значение блока
+    
+        self.arr.each do |element|  # Итерируемся по каждому элементу
+            current_value = yield(element)  # Получаем значение блока для текущего элемента
+    
+            # Если это первый элемент или значение изменилось, сохраняем предыдущую группу
+            if previous_value.nil? || current_value != previous_value
+                collections << [current_value, []]  # Начинаем новую группу
+                current_group = collections.last.last  # Устанавливаем текущую группу
+            end
+    
+            current_group << element  # Добавляем элемент в текущую группу
+            previous_value = current_value  # Обновляем предыдущее значение
+        end
+    
+        collections  # Возвращаем массив групп
     end
-
-
-
-
+    
 end
 
 new_arr = [1,2,3,4,5]
 
 arrProc = ArrProc.new(new_arr)
 
-puts(arrProc.filter {|num| num > 3})
-puts(arrProc.member?(3))
-puts(arrProc.member?(20))
+# puts(arrProc.filter {|num| num > 3})
+# puts(arrProc.member?(3))
+# puts(arrProc.member?(20))
 
-puts (arrProc.sum {|num| num.even?})
+# puts (arrProc.sum {|num| num.even?})
 
-puts arrProc.reduce(1) {|acc, num| acc * num}
-puts arrProc.reduce(1) {|acc, num| acc + num}
+# puts arrProc.reduce(1) {|acc, num| acc * num}
+# puts arrProc.reduce(1) {|acc, num| acc + num}
 
-puts arrProc.include? {|num| num > 5}
+# puts arrProc.include? {|num| num > 5}
+
+puts arrProc.chunk {|num| num.even?}.inspect
