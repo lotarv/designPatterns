54e0430e270b4823b11f9e5397c0f4f281d79eff
lotarv <lotarev200445@gmail.com>
2024-10-17 17:45:12 +0300

    lab3 3.3 class Tag

---
+++ lab3 - develop_concepts/html.rb
@@ -0,0 +1,53 @@
+class Tag
+    attr_accessor :name, :attributes, :children, :content
+    
+    def initialize(name:, attributes: {}, children: [], content: "")
+      self.name = name
+      self.attributes = attributes
+      self.children = children
+      self.content = content
+    end
+  
+    def opening_tag
+      attrs = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ") unless attributes.empty?
+
+      return "<#{name} #{attrs}>#{content}</#{name}>"
+    end
+
+    def closing_tag
+        if self.closing?
+            attrs = attributes.map { |key, value| "#{key}=\"#{value}\"" }.join(" ") unless attributes.empty?
+            return "</#{name} #{attrs}>"
+        end
+    end
+
+    def closing?
+        return ["img", "input"].member?(name)
+    end
+
+    def children_count
+        children.length
+    end
+
+    def closing?
+        ["img", "input"].include?(self.name)
+    end
+
+    def add_child(child)
+        self.children << child
+    end
+
+    def self.parse_attributes(attributes_str)
+        attributes = {}
+        attributes_str.scan(/([a-zA-Z]+)="([^" >]*)"/) do |key, value|
+            attributes[key] = value
+        end
+        attributes
+    end
+
+
+  end
+ 
+  testing = Tag.new(name: "h1", attributes: {height: "20px", width: "50px"}, content: "welcome")
+  puts testing.closing?
+  puts testing.opening_tag
