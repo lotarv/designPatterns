8a74c1d93c973a0b97dbc8ff18be4b44622b26ab
lotarv <lotarev200445@gmail.com>
2024-11-20 14:47:10 +0300

    lab4 task2.2 add_student() method implemented

--- Students/model classes/student_list_json.rb
+++ Students/model classes/student_list_json.rb
@@ -46,10 +46,38 @@ class Student_list_JSON
         @data.sort_by! {|data_obj| Student.new(data_obj).get_full_name}
     end
 
+    def add_student(student_obj)
+        data_obj = get_data_from_student_obj(student_obj)
+        data_obj[:id] = self.get_max_id + 1
+        @data.push(data_obj)
+    end
+
+    def replace_student(student_obj)
+    end
+
     def print_content()
         @data.each do |element|
             puts element
         end
+    end
+
+    
+    private 
+    def get_data_from_student_obj(student_obj)
+        data_obj = {
+            id: student_obj.id,
+            name: student_obj.name,
+            surname: student_obj.surname,
+            middle_name: student_obj.middle_name,
+            phone: student_obj.phone,
+            telegram: student_obj.telegram,
+            email: student_obj.email,
+            git: student_obj.git
+        }
+    end
+    def get_max_id()
+        max_id_element = @data.max_by {|data_obj| data_obj[:id]}
+        return max_id_element[:id]
     end
 end
 
@@ -57,8 +85,17 @@ json_list = Student_list_JSON.new()
 json_list.read("students.json")
 # data_list = json_list.get_k_n_student_short_list(20,2)
 
+student1 = Student.new({
+  id:5,
+  name: "Амаль",
+  surname: "Блягоз",
+  middle_name: "Хазретович",
+  phone: '88005553535',
+  telegram: '@lamafout',
+  email: 'lotarev.serge@yandex.ru',
+  git: 'https://github.com/lotarv'
+})
 json_list.sort()
+json_list.add_student(student1)
 
 json_list.print_content()
-
-puts json_list.get_as_students_obj()
