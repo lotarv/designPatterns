60e11ae4946d0f6de3929214a8383eba5067a289
lotarv <lotarev200445@gmail.com>
2024-11-22 13:00:45 +0300

    lab4 task3 main.rb for demonstration

---
+++ Students/database-connection/main.rb
@@ -0,0 +1,34 @@
+# main.rb
+
+require_relative 'student_list_DB.rb'
+
+# Создание объекта Student_list_DB
+student_list_db = Student_list_DB.new
+
+# Создание нового студента
+student = Student.new({
+  name: "Алёна",
+  surname: "Пономарева",
+  middle_name: "Игоревна",
+  phone: '87007007070',
+  email: 'ponomareva.alena@yandex.ru',
+  git: 'https://github.com/alenka228'
+})
+
+# Добавление студента в базу данных и список
+student_list_db.add_student(student)
+
+# Показать всех студентов после добавления
+puts "All students after adding one:"
+puts student_list_db.get_k_n_student_short_list(30,1).get_data()
+
+
+# Удалить студента
+student_list_db.delete_student(31)
+
+# Показать всех студентов после удаления
+puts "All students after deletion:"
+puts student_list_db.get_k_n_student_short_list(30, 1).get_data()
+
+# Получение количества студентов
+puts "Total number of students: #{student_list_db.count}"
