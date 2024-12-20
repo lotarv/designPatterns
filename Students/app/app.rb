0ff4434f07ac9ec69bf64e528f6b09aa9abc3567
lotarv <lotarev200445@gmail.com>
2024-12-20 20:43:04 +0300

    lab5 task 0.5 made filter section

--- Students/app/app.rb
+++ Students/app/app.rb
@@ -12,9 +12,7 @@ class MainWindow < FXMainWindow
 
     # Область фильтрации
     filter_frame = FXVerticalFrame.new(main_frame, FRAME_SUNKEN | LAYOUT_FILL_Y | LAYOUT_FIX_WIDTH, width: 200)
-    FXLabel.new(filter_frame, "Фильтрация")
-    FXTextField.new(filter_frame, 20, nil, 0, TEXTFIELD_NORMAL | LAYOUT_FILL_X)
-    FXButton.new(filter_frame, "Применить фильтр", opts: BUTTON_NORMAL | LAYOUT_FILL_X)
+    make_and_fill_filtration(filter_frame)
 
     # Область с таблицей
     table_frame = FXVerticalFrame.new(main_frame, FRAME_SUNKEN | LAYOUT_FILL_X | LAYOUT_FILL_Y)
@@ -52,6 +50,51 @@ class MainWindow < FXMainWindow
         table.setItemText(row, 3, "Описание для элемента #{row + 1}")
     end
   end
+
+  def make_and_fill_filtration(filter_frame)
+    FXLabel.new(filter_frame, "Фамилия и инициалы:")
+    FXTextField.new(filter_frame, 20, nil, 0, TEXTFIELD_NORMAL | LAYOUT_FILL_X)
+    # Поле фильтрации по git
+    add_filter_section(filter_frame, "Git")
+
+    # Поле фильтрации по почте
+    add_filter_section(filter_frame, "Почта")
+
+    # Поле фильтрации по телефону
+    add_filter_section(filter_frame, "Телефон")
+
+    # Поле фильтрации по Telegram
+    add_filter_section(filter_frame, "Telegram")
+  end
+
+  def add_filter_section(parent, label_text)
+    section_frame = FXVerticalFrame.new(parent, LAYOUT_FILL_X | LAYOUT_FIX_HEIGHT | FRAME_THICK,height:80)
+    FXLabel.new(section_frame, label_text)
+  
+    # Выпадающий список (ComboBox)
+    combo_box = FXComboBox.new(section_frame, 3, nil, 0, COMBOBOX_STATIC | LAYOUT_FILL_X)
+    combo_box.numVisible = 3
+    combo_box.appendItem("Не важно")
+    combo_box.appendItem("Да")
+    combo_box.appendItem("Нет")
+    combo_box.setCurrentItem(0) # По умолчанию выбрано "Не важно"
+  
+    # Поле ввода
+    input_field = FXTextField.new(section_frame, 20, opts: TEXTFIELD_NORMAL | LAYOUT_FILL_X)
+    input_field.hide
+
+    # Обработчик изменения выбора в ComboBox
+    combo_box.connect(SEL_COMMAND) do
+      case combo_box.currentItem
+      when 1 # "Да"
+        input_field.show
+      else # "Нет" или "Не важно"
+        input_field.hide
+      end
+      section_frame.recalc
+    end
+  end
+
 end
 
 if __FILE__ == $0
